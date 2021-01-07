9599a061cd0fbf7ad33f5b92b5abe149dfd5a4af
Souaibou95 <60626083+Souaibou95@users.noreply.github.com>
2021-01-06 20:42:46 -0500

    Update tourData.js

--- src/Components/TourList/tourData.js
+++ src/Components/TourList/tourData.js
@@ -11,20 +11,20 @@ export const tourData = [
         city: "indiana",
         img:  "./img/indiana.jpg",
         name: "indiana pace tour",
-        info: "Indiana is a..."
+        info: "Indiana is a U.S. state in the Midwestern and Great Lakes regions of North America. It is the 38th-largest by area "
       },
       {
         id:3,
         city: "milano",
         img:  "./img/milano.jpg",
         name: "milano arma tour",
-        info: "Milano is a..."
+        info: "Milan, a metropolis in Italy's northern Lombardy region, is a global capital of fashion and design."
       },
       {
         id:4,
         city: "nice",
         img:  "./img/nice.jpg",
         name: "nice mr tour",
-        info: "Nice is a..."
+        info: "Nice, capital of the Alpes-Maritimes department on the French Riviera, sits on the pebbly shores of the Baie des Anges."
       }
 ]
